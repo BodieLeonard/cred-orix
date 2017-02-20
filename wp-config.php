3ebc18a86f5441a212262b7fc65d764a1f5f4407
Mark DeArmond <bobb.dearmond@gmail.com>
2017-02-20 14:20:46 -0600

    Removed MAMP port configuration

--- wp-config.php
+++ wp-config.php
@@ -31,7 +31,7 @@ define('DB_NAME', 'orix_local');
 
 define('DB_USER', 'root');
 define('DB_PASSWORD', 'root');
-define('DB_HOST', '127.0.0.1:8889');
+define('DB_HOST', '127.0.0.1');
 define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
 
