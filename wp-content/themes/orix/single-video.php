dbe3c29d8f42292f5eed2aceabfb24f9f7c14ad9
Bodie Leonard <bodie.dev@gmail.com>
2017-02-20 13:02:50 -0600

    edit video page

--- wp-content/themes/orix/single-video.php
+++ wp-content/themes/orix/single-video.php
@@ -12,22 +12,20 @@
 
 get_header(); 
 
-$filter = (isset($_REQUEST['filter'])) ? $_REQUEST['filter'] : "ORIX";
-$title = str_replace("-", " ", $filter);
-
+$video_path = get_post_meta($post->ID, 'video_path', true); 
 $post_home = get_post(3754);
 $secondThumb = MultiPostThumbnails::get_post_thumbnail_url( 'page', 'secondary-image', $post_home->ID	);
 ?>
 	
-	<?php get_template_part( 'content', 'video' ); ?>
+	
+	<?php include( locate_template( 'content-video.php', false, false ) ); ?>
 		
 	</div>
 
 	<div id="content" class="site-content">
 
 	<section class='centered '>
-			<h1><?php echo $title; ?> NEWSROOM</h1>
-            
+			<h1><?php echo $post->post_title; ?></h1>
 		</section>
 
 	<div id="primary" class="content-area">
@@ -35,20 +33,14 @@ $secondThumb = MultiPostThumbnails::get_post_thumbnail_url( 'page', 'secondary-i
 
 
 			<?php while ( have_posts() ) : the_post(); ?>
-				
-                
 
 				<div class="col-xs-12 col-md-3 pull-right">
 					<?php get_template_part( 'content', 'sidebar-video-archive' ); ?>
 				</div>
 
-				<?php get_template_part( 'content', 'newsroom-full' ); 
-					#get_template_part( 'content', 'sidebar-news-archive' );
-				?>
-
-                <?php $content = get_post_meta(5230, 'video_content', true); 
-                    echo $content;
-                ?>
+				<?php $content = get_post_meta($post->ID, 'video_content', true); 
+            echo $content;
+        ?>
 
 			<?php endwhile; // end of the loop. ?>
 
